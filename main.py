30b3f0077c6869e970a56ca788316890d8662f6f
doctorwhat1 <86233023+doctorwhat1@users.noreply.github.com>
2022-06-19 16:24:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(Number):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Number: {Number}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+
+print_hi('16')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
