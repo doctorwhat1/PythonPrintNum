00f427251b2baf2f9892fa954df2c7e3274e3e8b
doctorwhat1 <86233023+doctorwhat1@users.noreply.github.com>
2022-06-20 10:17:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-# This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(Number):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Number: {Number}')  # Press Ctrl+F8 to toggle the breakpoint.
-    return(f'Number: {Number}')
+def countHash(line, module=10):
+    total = 0
+    for i in line:
+        #myord=(ord(i)-ord("a"))
+        total = (26*total + (ord(i)-ord("a")) ) % module
+    return total
+def main(module,pattern,stream):
 
+    matches=[]
+    spurious=[]
 
-# Press the green button in the gutter to run the script.
+    patternHash=countHash(pattern)
 
-print_hi('16')
+    #print(countHash('uv'))
+
+    for i in range(0,1+len(stream)-len(pattern)):
+        kusok=stream[i:i+len(pattern)]
+        if (patternHash==countHash(kusok)):
+            #Collision. Spurious?
+
+            if (kusok==pattern):
+                #real
+                matches.append(i)
+            else:
+                #sus
+                spurious.append(i)
+
+   # print("Matches: ",end="")
+
+    print(str(matches))
+    return(str(matches))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
