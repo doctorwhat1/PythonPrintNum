ee474c29d48dd9118a3a7c4537c996241c3ab8c8
doctorwhat1 <86233023+doctorwhat1@users.noreply.github.com>
2022-06-20 10:16:53 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
 import main
 
-def test_16():
-    assert main.print_hi('16') == "Number: 16"
+def test_1():
+    assert main.main(2,'vx','uvdhztxzgvepqnumjslrpwfjuoikuvxhjqevtpkmblroevezlnjvxhctjeujxcqexkyndvxfxxmkguoxrfnvkvxasjuwcbcdneaobkisnrvvadqwyqvxidszoywxrouriazfzavnfkvuouqtygvxobspgvpriigkfbcvpqfvxdxbugyhswswfconuhkrjxszehqxvxtdjpnglzesjn') == '[29, 51, 69, 85, 114, 146, 167, 196]'
